2e9122334ec1ca69c6c7d2a8f239e52ad379cad1
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-04-19 15:57:22 -0300

    polishing

--- metaclass.model/src/main/java/org/nanotek/meta/model/rdbms/JoinTableClassifier.java
+++ metaclass.model/src/main/java/org/nanotek/meta/model/rdbms/JoinTableClassifier.java
@@ -17,7 +17,7 @@ public class JoinTableClassifier extends MetaClassClassifier<RdbmsMetaClass> {
 			return Optional.of (classified);
 		return Optional.<RdbmsMetaClass>empty();
 	}
-//TODO: optimize this algorithm
+//TODO: optimize this algorithm/need to verify also if each foreign key has 1 attribute linked (in case of single attribute primary key).
 	private boolean verifyClassForeignKeys(RdbmsMetaClass classified) {
 		int counter = classified.getRdbmsForeignKeys().size();
 		if(counter>0 && classified.getRdbmsForeignKeys().size() == classified.getMetaAttributes().size()){
