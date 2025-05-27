f1feeccf52f727531aa8348f95773aaa8f7aebac
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-05-27 07:50:30 -0300

    removed inned enumeration for class export

--- metaclass.model/src/main/java/org/nanotek/meta/model/classification/MetaClassIdentityClassification.java
+++ metaclass.model/src/main/java/org/nanotek/meta/model/classification/MetaClassIdentityClassification.java
@@ -1,6 +1,5 @@
 package org.nanotek.meta.model.classification;
 
-import org.nanotek.meta.model.classification.MetaClassIdentityClassifier.KeyClassification;
 
 public record MetaClassIdentityClassification(KeyClassification type) {
 }
